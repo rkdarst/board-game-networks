9b98ff6cd0c144198e433439335c13f4ebb6e0cf
Richard Darst <rkd@zgib.net>
2020-02-07 21:02:21 +0200

    ticket-to-ride/united-states: new graph, new edgelist format

--- parse.py
+++ parse.py
@@ -14,46 +14,68 @@ def generate(fname):
     else:
         G = networkx.Graph()
 
+    node_degrees = { }
     for i, node in enumerate(net['nodes']):
-        G.add_node(node['label'], id=i, **node.get('labels', {}))
+        label = node['label']
+        attrs = dict(node.get('labels', {}))
+        if 'deg' in node:
+            node_degrees[label] = node['deg']
+        G.add_node(label, id=i, **node.get('labels', {}))
 
-    edges = collections.defaultdict(int)
-    edges_weights = collections.defaultdict(list)
-    for j, edge in enumerate(net['adjacency']):
-        a = edge['src']
+    if 'adjacency' in net:
+        edges = collections.defaultdict(int)
+        edges_weights = collections.defaultdict(list)
+        for j, edge in enumerate(net['adjacency']):
+            a = edge['src']
 
-        # weights or other data:
-        for b in edge['dst']:
-            if isinstance(b, list):
-                a_label = a['label']
-                b_label = b[0]['label']
-                weight = b[1]
-                if len(b) > 2:
-                    attrs = dict(b[2])   # making a copy of dict
+            # weights or other data:
+            for b in edge['dst']:
+                if isinstance(b, list):
+                    a_label = a['label']
+                    b_label = b[0]['label']
+                    weight = b[1]
+                    if len(b) > 2:
+                        attrs = dict(b[2])   # making a copy of dict
+                    else:
+                        attrs = { }
+                    if weight is not None:
+                        attrs['weight'] = weight
+                    G.add_edge(a_label, b_label, **attrs)
+                    edges[frozenset((a_label, b_label))] += 1
+                    edges_weights[frozenset((a_label, b_label))].append(weight)
                 else:
-                    attrs = { }
-                if weight is not None:
-                    attrs['weight'] = weight
-                G.add_edge(a_label, b_label, **attrs)
-                edges[frozenset((a_label, b_label))] += 1
-                edges_weights[frozenset((a_label, b_label))].append(weight)
-            else:
-                G.add_edge(a['label'], b['label'])
-                edges[frozenset((a['label'], b['label']))] += 1
+                    G.add_edge(a['label'], b['label'])
+                    edges[frozenset((a['label'], b['label']))] += 1
 
-    # Check the graph
+        # Check the graph
 
-    # Each edge is bi-directional
-    for k, v in edges.items():
-        if v != 2:
-            print("WARN: %s is not bidirectional (n=%s)"%(k, v))
-        #assert v == 2, "%s is not bidirectional"%(k, )
+        # Each edge is bi-directional
+        for k, v in edges.items():
+            if v != 2:
+                print("WARN: %s is not bidirectional (n=%s)"%(k, v))
+            #assert v == 2, "%s is not bidirectional"%(k, )
 
-    # Each edge has same weight both ways
-    for k, v in edges_weights.items():
-        #print(k, v)
-        if len(v) == 2 and v[0] != v[1]:
-            print("WARN: %s does not have the same weights both ways"%(k, ))
+        # Each edge has same weight both ways
+        for k, v in edges_weights.items():
+            #print(k, v)
+            if len(v) == 2 and v[0] != v[1]:
+                print("WARN: %s does not have the same weights both ways"%(k, ))
+
+    elif 'edges' in net:
+        for a, b, weight, labels in net['edges']:
+            a_label = a['label']
+            b_label = b['label']
+            if not isinstance(G, networkx.MultiGraph):
+                assert not G.has_edge(a_label, b_label), \
+                  "Graph already has edge (%s, %s)"%(a_label, b_label)
+            attrs = dict(labels)
+            if weight is not None:
+                attrs['weight'] = weight
+            G.add_edge(a_label, b_label, **attrs)
+
+        # Check degrees
+        for node, expected_degree in node_degrees.items():
+            assert G.degree(node) == expected_degree, "Node %s has wrong degree"%(node,)
 
     # no self-loops
     assert len(list(G.nodes_with_selfloops())) == 0, "Self loops in %s"%list(G.nodes_with_selfloops())
@@ -76,6 +98,13 @@ def main(argv):
         print("avg degree=%.2f"%(len(G.edges())*2/len(G.nodes())))
         print("density=%.3g"%(networkx.density(G)))
         print("transitivity=%.3g"%(networkx.transitivity(networkx.Graph(G))))
+        print("avg shortest path length=%.3g"%(networkx.average_shortest_path_length(networkx.Graph(G))))
+        print("avg shortest path length (weighted)=%.3g"%(networkx.average_shortest_path_length(networkx.Graph(G), weight='weight')))
+        print("diameter=%.3g"%(networkx.diameter(networkx.Graph(G))))
+        # This roundabout path is needed to compute diameter with weights
+        shortestpaths = networkx.shortest_path_length(G, weight='weight')
+        diameter = max(max(x[1].values()) for x in shortestpaths)
+        print("diameter (weighted)=%.3g"%(diameter, ))
         node_keys = set().union(*[list(x.keys()) for x in dict(G.nodes(data=True)).values()])
         print("node keys: %s"%(node_keys,))
         edge_keys = set().union(*[list(x[2].keys()) for x in G.edges(data=True)])
